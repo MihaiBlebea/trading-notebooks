f99a7f627e7261fe8dc17c31fe4a2891b49c3367
Mihai Blebea <mihaiserban.blebea@gmail.com>
2022-03-28 23:23:35 +0100

    Adds full html

--- export_notebook.sh
+++ export_notebook.sh
@@ -3,4 +3,4 @@
 export PYTHONDONTWRITEBYTECODE=1
 export PYTHONPATH="${PYTHONPATH}:${PWD}"
 # eval "./env/bin/python3 -u ${1} ${@:2}"
-./env/bin/jupyter nbconvert --execute --to html --template basic --output-dir=./output ${1}
+./env/bin/jupyter nbconvert --execute --to html --template full --output-dir=./output ${1}
